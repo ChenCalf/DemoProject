76ea998bedb44a2a3a4908c4a51156dc01ad554e
“ChenCalf <calfchen_qx@163.com>
2020-06-21 14:04:47 +0800

    添加了心累

---
+++ src/main/java/DemoClass.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DemoClass {
+
+    public static void main(String[] args) {
+        HashMap<String, Object> map = new HashMap<>();
+        Map<String, Object> synchronizedMap = Collections.synchronizedMap(map);
+        System.out.println("aaas");
+        System.out.println("一岁一枯荣");
+
+    }
+
+    protected void eat() throws Throwable {
+    }
+}
